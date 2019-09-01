46c0f747e5a849e7687be08daec89236d024d3d5
cristian david tilano silva <cristiantilanosilva@gmail.com>
2019-09-01 12:55:34 -0500

    ejercicio resuelto en clase del taller 1

---
+++ ejercicio1/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace ejercicio1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.ptbimagen5 = new System.Windows.Forms.PictureBox();
+            this.ptbimagen4 = new System.Windows.Forms.PictureBox();
+            this.ptbimagen3 = new System.Windows.Forms.PictureBox();
+            this.ptbimagen2 = new System.Windows.Forms.PictureBox();
+            this.ptbimagen1 = new System.Windows.Forms.PictureBox();
+            this.btnocultar = new System.Windows.Forms.Button();
+            this.btnmostrar = new System.Windows.Forms.Button();
+            this.txtimagen = new System.Windows.Forms.TextBox();
+            this.btntodo = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbimagen5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbimagen4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbimagen3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbimagen2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbimagen1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ptbimagen5
+            // 
+            this.ptbimagen5.Image = global::ejercicio1.Properties.Resources.LOGO_PATROCINADORES_NAL_ARESS_BLANCO_1024x1024;
+            this.ptbimagen5.Location = new System.Drawing.Point(583, 274);
+            this.ptbimagen5.Name = "ptbimagen5";
+            this.ptbimagen5.Size = new System.Drawing.Size(206, 177);
+            this.ptbimagen5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptbimagen5.TabIndex = 4;
+            this.ptbimagen5.TabStop = false;
+            this.ptbimagen5.DoubleClick += new System.EventHandler(this.ptbimagen5_DoubleClick);
+            // 
+            // ptbimagen4
+            // 
+            this.ptbimagen4.Image = global::ejercicio1.Properties.Resources.kisspng_under_armour_logo_clothing_nyse_uaa_5b07440a58dcd1_520342961527202826364;
+            this.ptbimagen4.Location = new System.Drawing.Point(582, 2);
+            this.ptbimagen4.Name = "ptbimagen4";
+            this.ptbimagen4.Size = new System.Drawing.Size(207, 172);
+            this.ptbimagen4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptbimagen4.TabIndex = 3;
+            this.ptbimagen4.TabStop = false;
+            this.ptbimagen4.DoubleClick += new System.EventHandler(this.ptbimagen4_DoubleClick);
+            // 
+            // ptbimagen3
+            // 
+            this.ptbimagen3.Image = global::ejercicio1.Properties.Resources.kisspng_the_coca_cola_company_logo_vector_graphics_brand_coca_cola_logo_design_icons_vector_free_download_5b7e590c1acce7_6105506115350069881098;
+            this.ptbimagen3.Location = new System.Drawing.Point(3, 274);
+            this.ptbimagen3.Name = "ptbimagen3";
+            this.ptbimagen3.Size = new System.Drawing.Size(201, 176);
+            this.ptbimagen3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptbimagen3.TabIndex = 2;
+            this.ptbimagen3.TabStop = false;
+            this.ptbimagen3.Click += new System.EventHandler(this.pictureBox3_Click);
+            this.ptbimagen3.DoubleClick += new System.EventHandler(this.ptbimagen3_DoubleClick);
+            // 
+            // ptbimagen2
+            // 
+            this.ptbimagen2.Image = global::ejercicio1.Properties.Resources.adidas_logo_transparent_background_20;
+            this.ptbimagen2.Location = new System.Drawing.Point(3, 2);
+            this.ptbimagen2.Name = "ptbimagen2";
+            this.ptbimagen2.Size = new System.Drawing.Size(237, 194);
+            this.ptbimagen2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptbimagen2.TabIndex = 1;
+            this.ptbimagen2.TabStop = false;
+            this.ptbimagen2.DoubleClick += new System.EventHandler(this.ptbimagen2_DoubleClick);
+            // 
+            // ptbimagen1
+            // 
+            this.ptbimagen1.Image = global::ejercicio1.Properties.Resources.kisspng_atltico_nacional_2018_florida_cup_copa_libertado_atletico_nacional_5b237fcd6b3183_9146040015290531334391;
+            this.ptbimagen1.Location = new System.Drawing.Point(287, 25);
+            this.ptbimagen1.Name = "ptbimagen1";
+            this.ptbimagen1.Size = new System.Drawing.Size(257, 217);
+            this.ptbimagen1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptbimagen1.TabIndex = 0;
+            this.ptbimagen1.TabStop = false;
+            this.ptbimagen1.DoubleClick += new System.EventHandler(this.ptbimagen1_DoubleClick);
+            // 
+            // btnocultar
+            // 
+            this.btnocultar.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnocultar.Location = new System.Drawing.Point(250, 295);
+            this.btnocultar.Name = "btnocultar";
+            this.btnocultar.Size = new System.Drawing.Size(108, 31);
+            this.btnocultar.TabIndex = 1;
+            this.btnocultar.Text = "Ocultar";
+            this.btnocultar.UseVisualStyleBackColor = true;
+            this.btnocultar.Click += new System.EventHandler(this.btnocultar_Click);
+            // 
+            // btnmostrar
+            // 
+            this.btnmostrar.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnmostrar.Location = new System.Drawing.Point(250, 347);
+            this.btnmostrar.Name = "btnmostrar";
+            this.btnmostrar.Size = new System.Drawing.Size(108, 29);
+            this.btnmostrar.TabIndex = 2;
+            this.btnmostrar.Text = "Mostrar";
+            this.btnmostrar.UseVisualStyleBackColor = true;
+            this.btnmostrar.Click += new System.EventHandler(this.btnmostrar_Click);
+            // 
+            // txtimagen
+            // 
+            this.txtimagen.Font = new System.Drawing.Font("Times New Roman", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtimagen.Location = new System.Drawing.Point(400, 295);
+            this.txtimagen.Name = "txtimagen";
+            this.txtimagen.Size = new System.Drawing.Size(134, 81);
+            this.txtimagen.TabIndex = 0;
+            // 
+            // btntodo
+            // 
+            this.btntodo.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btntodo.Location = new System.Drawing.Point(221, 393);
+            this.btntodo.Name = "btntodo";
+            this.btntodo.Size = new System.Drawing.Size(172, 33);
+            this.btntodo.TabIndex = 5;
+            this.btntodo.Text = "Mostrar todos";
+            this.btntodo.UseVisualStyleBackColor = true;
+            this.btntodo.Click += new System.EventHandler(this.btntodo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btntodo);
+            this.Controls.Add(this.txtimagen);
+            this.Controls.Add(this.btnmostrar);
+            this.Controls.Add(this.btnocultar);
+            this.Controls.Add(this.ptbimagen5);
+            this.Controls.Add(this.ptbimagen4);
+            this.Controls.Add(this.ptbimagen3);
+            this.Controls.Add(this.ptbimagen2);
+            this.Controls.Add(this.ptbimagen1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ptbimagen5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbimagen4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbimagen3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbimagen2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbimagen1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.PictureBox ptbimagen1;
+        private System.Windows.Forms.PictureBox ptbimagen2;
+        private System.Windows.Forms.PictureBox ptbimagen3;
+        private System.Windows.Forms.PictureBox ptbimagen4;
+        private System.Windows.Forms.PictureBox ptbimagen5;
+        private System.Windows.Forms.Button btnocultar;
+        private System.Windows.Forms.Button btnmostrar;
+        private System.Windows.Forms.TextBox txtimagen;
+        private System.Windows.Forms.Button btntodo;
+    }
+}
+
---
+++ ejercicio1/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejercicio1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtimagen.Focus();
+        }
+
+        private void btnocultar_Click(object sender, EventArgs e)
+        {
+            string numeroImagen = txtimagen.Text;
+
+            switch (numeroImagen)
+            {
+                case "1":
+                    ptbimagen1.Visible = false;
+                    break;
+                case "2":
+                    ptbimagen2.Visible = false;
+                    break;
+                case "3":
+                    ptbimagen3.Visible = false;
+                    break;
+                case "4":
+                    ptbimagen4.Visible = false;
+                    break;
+                case "5":
+                    ptbimagen5.Visible = false;
+                    break;
+                default:
+                    MessageBox.Show("Ingrese un numero del 1 al 5.");
+
+                    break;
+
+
+            }
+            restablecerCampo();
+        }
+
+        private void btnmostrar_Click(object sender, EventArgs e)
+        {
+            string numeroImagen = txtimagen.Text;
+
+            switch (numeroImagen)
+            {
+                case "1":
+                    ptbimagen1.Visible = true;
+
+                    break;
+                case "2":
+                    ptbimagen2.Visible = true;
+
+                    break;
+                case "3":
+                    ptbimagen3.Visible = true;
+
+                    break;
+                case "4":
+                    ptbimagen4.Visible = true;
+
+                    break;
+                case "5":
+                    ptbimagen5.Visible = true;
+
+                    break;
+                default:
+                    MessageBox.Show("Ingrese un numero del 1 al 5.");
+
+                    break;
+            }
+
+            restablecerCampo();
+        }
+
+        private void restablecerCampo()
+        {
+
+            txtimagen.Text = "";
+            txtimagen.Focus();
+
+        }
+
+        private void ptbimagen1_DoubleClick(object sender, EventArgs e)
+        {
+            ptbimagen1.Visible = false;
+            abrirUrl("https://www.atlnacional.com/");
+        }
+
+        private void ptbimagen2_DoubleClick(object sender, EventArgs e)
+        {
+            ptbimagen2.Visible = false;
+            abrirUrl("https://www.adidas.com/");
+        }
+
+        private void ptbimagen3_DoubleClick(object sender, EventArgs e)
+        {
+            ptbimagen3.Visible = false;
+            abrirUrl("https://www.cocacola.com/");
+        }
+
+        private void ptbimagen4_DoubleClick(object sender, EventArgs e)
+        {
+            ptbimagen4.Visible = false;
+            abrirUrl("https://www.underarmour.com/");
+        }
+
+        private void ptbimagen5_DoubleClick(object sender, EventArgs e)
+        {
+            ptbimagen5.Visible = false;
+            abrirUrl("https://www.gmail.com/");
+        }
+
+        private void abrirUrl(string pagina)
+        {
+           System.Diagnostics.Process.Start(pagina);
+        }
+
+        private void btntodo_Click(object sender, EventArgs e)
+        {
+            ptbimagen1.Visible = true;
+            ptbimagen2.Visible = true;
+            ptbimagen3.Visible = true;
+            ptbimagen4.Visible = true;
+            ptbimagen5.Visible = true;
+        }
+    }
+}
